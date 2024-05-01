5b6d1e51fa8ff518b8217ce192857dc1b456dc8f
Jhakay <59739330+Jhakay@users.noreply.github.com>
2024-05-01 13:40:26 +0100

    Delete config.js
    Sensitive information removed.

--- config.js
+++
@@ -1,3 +0,0 @@
-export const GoogleBooksAPI_Key = 'AIzaSyCGpxRBQLIREEH_Usnc4gAc9MAmkFiE918';
-export const rawgAPI_Key = '40ff7ec3164b43878fefd400e6276f38';
-export const tmdbAPI_Key = '840d547834e96b2d250a21396a99581b';
